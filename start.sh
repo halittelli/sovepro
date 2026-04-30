8c0c0a768d0e77e4a8a183300e6302b381181ee3
halittelli <142510140+halittelli@users.noreply.github.com>
2026-04-30 11:21:21 +0300

    Add start script for running Streamlit app

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+streamlit run streamlit_app.py \
+  --server.port $PORT \
+  --server.address 0.0.0.0 \
+  --server.headless=true \
+  --server.enableCORS=false \
+  --server.enableXsrfProtection=false \
+  --browser.gatherUsageStats=false
